47ae5acc47462bdf78fc13d07fa10dafd25e9bc5
Patryk <Patryk@DESKTOP-CIEAV1R>
2024-12-12 00:10:04 +0100

    Dodaj pliki projektów.

---
+++ rzutkostka/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace rzutkostka
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
